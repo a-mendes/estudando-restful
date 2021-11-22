f5d7211d578d4befdfcb60ecf176876256c923e7
Ana Laura <88839593+a-mendes@users.noreply.github.com>
2021-11-22 17:48:46 -0300

    Spring Initializer
    Auto-genereted code by start.spring.io

---
+++ src/main/java/br/com/projetorestful/estudandorestful/EstudandoRestfulApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetorestful.estudandorestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudandoRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudandoRestfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projetorestful/estudandorestful/EstudandoRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetorestful.estudandorestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudandoRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
