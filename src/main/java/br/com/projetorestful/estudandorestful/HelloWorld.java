51cd4910aa49550f501842869d90a9ca74d3b0dc
Ana Laura <88839593+a-mendes@users.noreply.github.com>
2021-11-23 11:38:17 -0300

    Hello World, Ana!
    First API using rest

---
+++ src/main/java/br/com/projetorestful/estudandorestful/HelloWorld.java
@@ -0,0 +1,16 @@
+package br.com.projetorestful.estudandorestful;
+
+public class HelloWorld {
+	
+	private String name;
+	
+	public HelloWorld(String name)
+	{
+		this.name = name;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+}
